bbecd9e63f3f729cd516b3e6e15b53213cbc223d
Elise Easter Christiansen <eec@dips.no>
2021-06-17 10:30:03 +0200

    Made a reuseable modal component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,33 @@
+
+
 <script>
+import Modal from './Modal.svelte'
+let showModal = false;
+
+const toggleModal = () => {
+	showModal = !showModal;
+}
 
 </script>
 
+<Modal header="Information" message="Here is some information" isInfo={true} {showModal} on:click={toggleModal}/>
 <main>
-	
+	<button on:click={toggleModal}>Open modal</button>
 </main>
 
 <style>
+
+	main{
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media(min-width: 640px){
+		main{
+			max-width: none;
+		}
+	}
 
 </style>
---
+++ src/Modal.svelte
@@ -0,0 +1,40 @@
+<script>
+    export let showModal = false;
+    export let isInfo = false;
+    export let message = "Default value";
+    export let header = "Default value";
+
+</script>
+
+<!-- If showModal is true then show it -->
+{#if showModal}
+    <div class="backdrop" class:info={isInfo} on:click|self>
+        <div class="modal">
+            <h3>{header}</h3>
+            <p>{message}</p>
+        </div>
+    </div>
+{/if}
+    
+
+<style>
+    .backdrop{
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        background: rgba(0,0,0,0.8);
+    }
+
+    .modal{
+        padding: 10px;
+        border-radius: 10px;
+        background-color: white;
+        margin: 10% auto;
+        text-align: center;
+        max-width: 400px;
+    }
+
+    .info .modal{
+        background-color: tomato;
+    }
+</style>
